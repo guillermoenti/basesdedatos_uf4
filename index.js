f5a4cdbf450da6b161527ab8dc5f67e3b049d7b1
Guillermo Barrasa <guillermo.barrasa@enti.cat>
2021-06-13 16:37:22 +0200

    chat

--- index.js
+++ index.js
@@ -16,14 +16,10 @@ mongo.connect(server_url, (err, server) => {
 	}
 
 	console.log("Dentro de MongoDB");
-
 	chat_db = server.db("amongmeme");
 });
 
-
-
 console.log("Inicializando servidor chat");
-
 
 let public_files = new node_static.Server("pub");
 
@@ -32,18 +28,14 @@ http.createServer( (request, response) => {
 	if (request.url.startsWith("/chat")){
 		let info = request.url.split("=");
 		console.log(info[1]);
-		
 		let query = {
 			date : { $gt : parseInt(info[1]) }
 		};
 
 		let cursor = chat_db.collection("chat").find(query);
-
 		cursor.toArray().then( (data) => {
 			response.writeHead(200, {'Content-Type': 'text/plain'});
-
 			response.write( JSON.stringify(data) );
-
 			response.end();
 		});
 
@@ -52,23 +44,17 @@ http.createServer( (request, response) => {
 
 	if (request.url == "/recent"){
 		const estimated_count = chat_db.collection("chat").estimatedDocumentCount();
-	
 		estimated_count.then( (count) => {
 			console.log(count);
-
 			const MAX = 5;
-
 			let cursor = chat_db.collection("chat").find({}, {
 					skip: count - MAX,
 					limit: MAX,
 					sort: { $natural:1 }
 				});
-	
 			cursor.toArray().then( (data) => {
 				response.writeHead(200, {'Content-Type': 'text/plain'});
-
 				response.write( JSON.stringify(data) );
-
 				response.end();
 			});
 
@@ -79,16 +65,11 @@ http.createServer( (request, response) => {
 
 	if (request.url == "/submit"){
 		console.log("Envío de datos");
-
 		let body = [];
 		request.on('data', (chunk) => {
-
 			body.push(chunk);
-
 		}).on('end', () => {
-			
 			let chat_data = JSON.parse(Buffer.concat(body).toString());
-
 			chat_db.collection("chat").insertOne({
 				user: chat_data.chat_user,
 				msg: chat_data.chat_msg,
@@ -98,7 +79,6 @@ http.createServer( (request, response) => {
 		});
 
 		response.end();
-
 		return;
 	}
 	if(request.url == "/history"){
@@ -106,27 +86,23 @@ http.createServer( (request, response) => {
         let cursor = chat_db.collection("chat").find({},{
             sort:{$natural:1}
         });
-        
         let chat = cursor.toArray();
         let showText ="";
         chat.then((data) =>{
-            
             for(let i = 0; i<data.length;i++){
                 let ts = data[i].date;
                 ts = Number(ts);
-
                 let date = new Date(ts);
-                                
+                let dateText = date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear() +" "+ 
+                date.getHours() + ":" + date.getMinutes()+ ":" +  date.getSeconds();
                 showText = "<p>" + "[" +  dateText + "] - " + data[i].user + ": " + data[i].msg + "<p/>";
-                
+                console.log(showText);
                 response.write(showText);
             }
             
             response.end();
         })
     }
-
-
 
 	public_files.serve(request, response);
 
